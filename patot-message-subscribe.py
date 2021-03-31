69f2a432ceaf82c4204a16f0264bda12516ccc23
2ndyrGroupB <2ndyrGroupB@pn.local>
2021-03-31 20:24:33 +0800

    Added

---
+++ patot-message-subscribe.py
@@ -0,0 +1,16 @@
+import paho.mqtt.client as mqtt
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+    client.subscribe("eleasar/message")
+
+def on_message(client, userdata, msg):
+    print(str(msg.payload.decode()))
+
+client = mqtt.Client()
+client.on_connect = on_connect
+client.on_message = on_message
+
+client.connect("test.mosquitto.org", 1883, 60)
+
+client.loop_forever()
